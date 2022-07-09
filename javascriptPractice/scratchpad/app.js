b549e8e4cedbec17534824b81c8535f2cc7498e6
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-09 20:48:10 +0100

    coersion

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,1 +1,11 @@
 console.log(3 < 2 < 1);
+
+// due to associativity
+
+console.log(false < 1);
+
+// due to coercion ()use Number(false))
+
+console.log(0 < 1)
+
+// hence the boolean above becomes true
