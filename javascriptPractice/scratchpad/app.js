9d1400bf15558eab1e9e12a027a01a7a93371dab
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-10 18:59:07 +0100

    a function statement

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,11 +1,3 @@
 function greet(){
     console.log('hi');
 }
-
-greet.language = 'english';
-
-// but wait
-console.log(greet);
-
-// it seems we need to find property
-console.log(greet.language);
