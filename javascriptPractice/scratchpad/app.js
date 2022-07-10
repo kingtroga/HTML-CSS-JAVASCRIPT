e573235d01f57536cd2f878f0638c48066b87b0f
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-10 16:48:16 +0100

    get it

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,2 +1,10 @@
-var person = {};
+var person = { firstname: 'Tony', lastname: "Alicea"};
 console.log(person);
+
+// above is the same as 
+person = new Object();
+person.firstname = "Tony";
+person.lastname = "Alicea";
+console.log(person);
+
+// although the first way of writing was faster.
