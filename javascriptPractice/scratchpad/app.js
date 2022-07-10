7944c5a43983b1c500e7c0782f45bd80547988a7
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-10 20:16:25 +0100

    hoisting pop quiz

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,10 +1,15 @@
 greet();
 
+// function statement.
 function greet(){
     console.log('hi');
 }
+
+anonymousGreet();
+// what would be the value of the code above via hoisting?
 
 // function statement.
 var anonymousGreet = function() {
     console.log('hi');
 }
+
