e79f13c7f9592af30c81031a21b8de89e895709e
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-10 18:15:34 +0100

    How to fake Namespaces in Javascript

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -2,3 +2,13 @@ var greet = 'Hello!';
 var greet = 'Hola!';
 
 console.log(greet);
+
+// How to fake Namespaces in JavaScript
+
+var english = {};
+var spanish = {};
+
+english.greet = 'Hello!';
+spanish.greet = 'Hola!';
+
+console.log(english.greet);
