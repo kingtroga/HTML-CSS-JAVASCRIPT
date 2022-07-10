4b03ffbae3ad0a5e197fce6ed0d31dce2112bd47
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-10 18:48:07 +0100

    Just added a property to a func

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,11 +1,5 @@
-var objectLiteral = {
-    firstname: 'Mary',
-    isAProgrammer: true
-};
+function greet(){
+    console.log('hi');
+}
 
-console.log(JSON.stringify(objectLiteral));
-
-// How to turn a JSON into a JavaScript Object literal
-var jsonValue = JSON.parse('{"firstname": "Mary","isAProgrammer": true}');
-
-console.log(jsonValue);
+greet.language = 'english';
