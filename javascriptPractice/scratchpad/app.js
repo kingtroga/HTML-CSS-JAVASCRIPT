c9e29e63abfd9f2e44074f45d08e876ed9bc4260
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-11 22:39:27 +0100

    declaration

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,33 +1,5 @@
-// wethere it's a function statement
-function a() {
-    console.log(this);
-    this.newvariable = 'hello';
-}
+// declaration one
+var arr = new Array();
 
-// or it's a function expression
-var b = function() {
-     console.log(this);
-}
-
-// at that level of the code this will point to the globale object.
-a();
-console.log(newvariable);
-
-var c = {
-    log: function() {
-        var self = this; // since this are objects. Self is going to be 
-                        // point to the same location in memory as this.
-        self.name = 'Updated c object';
-        console.log(this);
-
-
-        // this here points to the global object...
-        var setname = function(newname) {
-            self.name = newname;
-        };
-        setname('Updated again! The c object');
-        console.log(self);
-    }
-}
-
-c.log();
+// declaration two
+var arr = [];
