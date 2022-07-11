2ab8c5f7943c644f3c32decda39b7e72b61d8da5
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-11 21:28:56 +0100

    this points to what

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,1 +1,12 @@
-console.log(this);
+// wethere it's a function statement
+function a() {
+    console.log(this);
+}
+
+// or it's a function expression
+var b = function() {
+     console.log(this);
+}
+
+// at that level of the code this will point to the globale object.
+a();
