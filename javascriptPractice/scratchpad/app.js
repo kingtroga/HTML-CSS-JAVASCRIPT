d9b529508504d1eb7e85a828f76d10f0372f6aa1
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-11 23:48:54 +0100

    why the code works

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -2,6 +2,12 @@ function greet(firstname, lastname, language ) {
 
     language = language || 'en'; //defualt hack javascripts)
 
+    if (arguments.length === 0)  {
+        console.log('Missing parameters!');
+        console.log('-------------------');
+        return;
+    }
+
     console.log(firstname);
     console.log(lastname);
     console.log(language);
