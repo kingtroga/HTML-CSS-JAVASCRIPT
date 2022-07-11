543d4ca85bcc7d589b95a132e4924787bf8365df
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-11 22:59:30 +0100

    the 'arguments' keyword.

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,22 +1,7 @@
-var arr = [
-            1, 
-            true, 
-            {
-                name: 'Tony',
-                address: '111 Main st.'
-            },
-            function(name) {
-                var greeting = 'Hello ';
-                console.log(greeting + name);
-            }, 
-            'hello'
-        ];
-// What the fuck i'm I sure that Python can do this?
-// JavaScript is like python on steriods.
+function greet(firstname, lastname, language) {
+    console.log(firstname);
+    console.log(lastname);
+    console.log(language);
+}
 
-
-console.log(arr);
-
-// Holy fuck
-arr[3](arr[2].name)
-
+greet();
