30c6ac4fc0b9371f9beb39941c22a779b05d6f41
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-12 12:08:45 +0100

    Dangerous Aside

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,24 +1,8 @@
-function greet(firstname, lastname, language) {
-    // what if I didn't always want to pass the language.
-
-    language = language || 'en';
-
-    if (language ==='en') {
-        console.log('Hello ' + firstname +  " " + lastname);
-    }
-
-    if (language ==='es') {
-        console.log('Hola ' + firstname +  " " + lastname);
+function getPerson() {
+    return
+    {
+        firstname: 'Tony'
     }
 }
 
-// open approach to function overloading
-function greetEnglish(firstname, lastname) {
-    greet(firstname, lastname, 'en');
-}
-
-function greetSpanish(firstname, lastname) {
-    greet(firstname, lastname, 'es');
-}
-greet('John', 'Doe', 'en');
-greet('John', 'Doe', 'es');
+console.log(getPerson());
