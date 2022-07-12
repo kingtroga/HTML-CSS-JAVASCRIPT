047d41db9323e8440e9c3308efb959805adbb456
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-12 12:42:50 +0100

    IIFE  V1

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,21 +1,6 @@
-var  
-     // firstname of the person
-     firstname,
-
-     // lastname of the person
-     lastname,
-
-     // the language
-     // can be 'en" or 'es'
-     language;
-
-var person = {
-      // the first name
-      firstname: 'John',
-
-      // the last name
-      // (always required)
-      lastname: 'Doe'
+// Function Statement
+function greet(name) {
+    console.log('Hello ' + name);
 };
+greet();
 
-console.log(person);
