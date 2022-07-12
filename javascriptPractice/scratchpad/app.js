d73a0aa4202c59c1b0d9d9f9620dc06e60d75f85
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-13 00:23:20 +0100

    functiional programming is very powerful...

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -16,10 +16,9 @@ function mapForEach(arr, fn) {
 var arr1 = [1, 2, 3]
 console.log(arr1);
 
-var arr2 = [];
-for (var i=0; i < arr1.length; i++) {
-
-    arr2.push(arr[i] * 2);
+var arr2 = mapForEach(arr1, function(item){
+    return item*2;
 }
+)
 
 console.log(arr2);
