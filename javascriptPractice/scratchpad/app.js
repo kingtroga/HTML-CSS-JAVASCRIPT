01a887e8328e888683ca3fdd074d9b8cc5e19515
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-12 18:53:16 +0100

    CLOSURES AND CALLBACKS.

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,22 +1,13 @@
-function makeGreeting(language) {
+function sayHiLater() {
 
-    return function(firstname, lastname) {
-        if (language === 'en') {
-            console.log('Hello' + firstname + ' ' + lastname);
-        }
+    var greeting = 'Hi!';
 
-        if (language === 'es') {
-            console.log('Hola' + firstname + ' ' + lastname);
-        }
-    }
+    setTimeout( function() { //CALLBACKS, FIRST CLASS FUNCTION, CLOSURE,
+                             // Anonymous functions.
+
+        console.log(greeting);
+    }, 3000);
+
 }
 
-// What do you think is going to happen here?
-var greetEnglish = makeGreeting('en'); // well a function with access to variables is returned
-var greetSpanish = makeGreeting('es');
-// Though these two functions lexically sit in the same make greeting. they
-// are going to point at two very different spots in memory because they were
-// created during different execution context.
-
-greetEnglish('John', 'Doe');
-greetSpanish('John', 'Doe'); // Here I have created a function  factories.
+sayHiLater();
