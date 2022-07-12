583a4d3b0d88001f4cd5eacccc921685330034f8
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-12 19:07:03 +0100

    callbacks

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -13,6 +13,28 @@ function sayHiLater() {
 sayHiLater();
 
 // jQuery uses function expressions and first-class functions!
-$('button').click(function () {
+/* $('button').click(function () {
 
-});
+}); */
+
+function tellMeWhenDone(callback) {
+    
+    var a = 1000; // some work
+    var b = 2000; // some work
+    console.log(a, b)
+    callback(); // the 'callback', it runs the function I give it.
+
+    
+}
+
+tellMeWhenDone(function(){
+
+    console.log('I am done!');
+
+})
+
+tellMeWhenDone(function(){
+
+    console.log('I am done!');
+
+})
