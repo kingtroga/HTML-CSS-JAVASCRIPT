4b421e7f361dc21f8a6526dcd30220008775c50b
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-13 12:36:22 +0100

    building objects

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,52 +1,9 @@
-var person = {
-    firstname: 'Default',
-    lastname: 'Default',
-    getFullName: function() {
-        return this.firstname + ' ' + this.lastname;
-    }
-}
+function Person() {
 
-var john = {
-    firstname: 'John',
-    lastname: 'Doe'
-}
-
-// don't do this EVER! for demo purposes only!!!
-john.__proto__ = person;
-
-// the for loop reach out and got every property that's not just
-// on the object but on the propototype itself.
-
-// what if I didn't want the object to reach out.
-// then.
-for (var prop in john) {
-    
-    if (john.hasOwnProperty(prop)) { // so I can reflect on the John objec
-                                     // reflect on it's properties
-                                     // this features lets use do something
-                                     // even cooler but it's not built into
-                                     // JavaScript. It's in Underscore though
-        console.log(prop + ': ' + john[prop]);
-    }
+    this.firstname = 'John';
+    this.lastname = 'Doe';
 
 }
 
-var jane = {
-    address: '111 Main St.',
-    getFormalFullName: function() {
-        return this.lastname + ', ' + this.firstname;
-    }
-}
-
-var jim =  {
-      getFirstName: function() {
-          return firstname;
-      }
-}
-
-_.extend(john,jane, jim); // this composes or combines these objects
-                          // it takes all the properties and methods of 
-                          // the object parameters and adds it to john 
-                          // for me. This is different from the prototype
-                          // chain this physically actually places the 
-                          // prototype in the john object
+var john = new Person();
+console.log(john);
