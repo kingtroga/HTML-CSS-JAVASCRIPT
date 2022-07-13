fe47f4811fdb6b725ace772b4d1d7a7f7932ef3c
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-13 14:40:21 +0100

    POLYFILL

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,3 +1,18 @@
+// POLYFILL>
+
+if (!Object.create) {
+    Object.create = function (o) {
+        if (arguments.length > 1) {
+            throw new Error('Object.create implementation' 
+            + " only accepts the first parameter.");
+        }
+        function F() {
+            F.prototype = o;
+            return new F();
+        };
+    }
+}
+
 var person = {
     firstname: 'Default',
     lastname: 'Default',
