281f323918111d24de2de1fb3bea239db90eebf5
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-13 20:33:00 +0100

    Strict mode v1

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,23 +1,4 @@
-var a = 3;
-console.log(typeof a);
+var person;
 
-var b = "Hello";
-console.log(typeof b);
-
-var c = {};
-console.log(typeof c);
-
-var d = [];
-console.log(typeof d);  // weird! why the course says why.
-console.log(Object.prototype.toString.call(d)); // better!
-
-function Person(name) {
-    this.name = name;
-}
-
-var e = new Person('Jane');
-console.log(typeof e);
-console.log(e instanceof Person);
-
-console.log(typeof undefined); // makes sense
-console.log(typeof null); // a bug since like forever
+persom = {};
+console.log(persom);
