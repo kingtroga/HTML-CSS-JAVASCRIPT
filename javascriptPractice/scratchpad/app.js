3b29029ac5fd4e54fed91a019c3ec990e22c6662
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-13 20:38:18 +0100

    strict mode end

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,6 +1,10 @@
 "use strict";
 // use strict either at the top of your file or at the top of a function.
+// not a 100% stuff because not every JavaScript engine processes it the
+// the same way.
 var person;
 
 person = {};
 console.log(persom);
+// here's a wonderful link that talks about how strict mode is implement
+// across javascript engines. (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode)
