0b4c6ffbe1769fba93c7f9ad760ea1e0dfb095c5
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-13 13:58:34 +0100

    for in v1

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -1,22 +1,5 @@
-function Person(firstname, lastname) {
+var arr = ['John', 'Jane', 'Jim'];
 
-    this.firstname = firstname;
-    this.lastname = lastname;
-
+for (var prop in arr) {
+    console.log(prop + ': ' + arr[prop] );
 }
-
-Person.prototype.getFullName = function () {
-    return this.firstname + ' ' + this.lastname;
-}
-
-var john = new Person('John', 'Doe');
-console.log(john);
-
-var jane = new Person('Jane', 'Doe');
-console.log(jane);
-
-Person.prototype.getFormalFullName = function () {
-    return this.lastname + ' ' + this.firstname;
-}
-
-console.log(john.getFormalFullName());
