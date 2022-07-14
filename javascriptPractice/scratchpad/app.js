86daed55523a2033b15034e17ceacf1c716be08e
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-14 12:14:29 +0100

    object creation

---
+++ javascriptPractice/scratchpad/app.js
@@ -0,0 +1,27 @@
+(function(global, $) {
+
+    var Greetr = function(firstname, lastname, language) {
+
+        return new Greetr.init(firstName, lastName, language);
+
+    }
+    
+    Greetr.prototype = {};
+
+    Greetr.init = function(firstName, lastName, language) {
+
+        var self = this;
+        self.firstName = firstName|| '';
+        self.lastName = lastName || '';
+        self.language = language || 'en';
+
+          
+    }
+
+    Greetr.init.prototype = Greetr.prototype;
+
+    
+
+
+
+}(window, jQuery));
