d069fe5c61d23d441e6a5359d43bcf09d3a27d6b
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-14 13:01:22 +0100

    made it chainable

--- javascriptPractice/scratchpad/app.js
+++ javascriptPractice/scratchpad/app.js
@@ -5,8 +5,64 @@
         return new Greetr.init(firstName, lastName, language);
 
     }
+
+    var supportedLangs = [ 'en', 'es'];
+
+    greetings = {
+        en: 'Hello',
+        es: 'Hola'
+    };
+
+    formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos'
+    };
+
+    var logMessages = {
+        en: 'Logged in',
+        es: 'Inicio sesion'
+    }
     
-    Greetr.prototype = {};
+    Greetr.prototype = {
+
+          fullName: function() {
+              return this.firstName + ' ' + this.lastName;
+          },
+
+          validate: function() {
+              if (supportedLangs.indexOf(this.language) === -1) {
+                  throw "Invalid language";
+              }
+          },
+
+          greeting: function() {
+              return greetings[this.language] + ' ' + this.firstName + '!';
+          },
+
+          formalGreeting: function() {
+              return formalGreetings[thislanguage] + ', ' + this.fullName();
+          },
+
+          greet: function(formal) {
+
+              var msg;
+              
+              // if undefined or null it will be coerced to 'false'
+              if (formal) {
+                  msg = this.formalGreeting();
+              }
+              else {
+                  msg = this.greeting();
+              }
+
+              if (console) {
+                  console.log(msg);
+              }
+
+              return this;
+          }
+
+    };
 
     Greetr.init = function(firstName, lastName, language) {
 
